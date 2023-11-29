557a323dae127f4c3741be6a0bfbd8126d6f1977
yrkash <yrkash@gmail.com>
2023-11-30 00:18:08 +0300

    Redis (working)

--- src/main/java/com/example/tasklist/TasklistApplication.java
+++ src/main/java/com/example/tasklist/TasklistApplication.java
@@ -2,7 +2,10 @@ package com.example.tasklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -13,6 +16,10 @@ public class TasklistApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(TasklistApplication.class, args);
+	}
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager();
 	}
 
 }
